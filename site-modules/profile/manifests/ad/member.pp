a0d0475e256041ea66540bea3d6bc3446a634bdb
Erik Hjelmås <erik.hjelmas@ntnu.no>
2020-02-10 21:11:18 +0100

    Update: Windows DNS only to dc1 since manager is in hosts

--- site-modules/profile/manifests/ad/member.pp
+++ site-modules/profile/manifests/ad/member.pp
@@ -1,19 +1,12 @@
 class profile::ad::member {
 
    $dc_password = Sensitive(lookup('profile::ad::server::password'))
-   $ifnames     = keys($::facts['networking']['interfaces'])
-   $dc1_ip  = dns_a('dc1')[0]
 
    reboot { 'dsc_reboot' :
      message => 'DSC has requested a reboot',
      when    => 'pending'
    }
 
-   dsc_dnsserveraddress { 'DnsServerAddress':
-      dsc_address        => "$dc1_ip",
-      dsc_interfacealias => "${ifnames[0]}",
-      dsc_addressfamily  => 'IPv4',
-   } ->
    dsc_user { 'Administrator':
      dsc_username    => 'Administrator',
      dsc_description => 'Administrator account',
