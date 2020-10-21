6f2821af0440e84a079fca0fb7d69872b2e08e9f
Wojciech Malecki <wojciecm@loginstud04.it.ntnu.no>
2020-10-21 13:14:34 +0200

    Delete unused roles

--- site-modules/role/manifests/domain_joined_client.pp
+++
@@ -1,7 +0,0 @@
-class role::domain_joined_client {
-  include ::profile::base_windows
-  include ::profile::dns::client
-  include ::profile::consul::client
-  include ::profile::sensu::agent_windows
-  include ::profile::ad::client
-}
--- site-modules/role/manifests/domain_joined_server.pp
+++
@@ -1,8 +0,0 @@
-class role::domain_joined_server {
-  include ::profile::base_windows
-  include ::profile::base_windows_server
-  include ::profile::dns::client
-  include ::profile::consul::client
-  include ::profile::sensu::agent_windows
-  include ::profile::ad::client
-}
