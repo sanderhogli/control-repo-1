fbcad05bbc3cfadb40cd44f2199523389d1e68ea
Monastyr <60261397+Monastyr@users.noreply.github.com>
2020-10-23 00:18:18 +0200

    Update manager_server.pp

--- site-modules/role/manifests/manager_server.pp
+++ site-modules/role/manifests/manager_server.pp
@@ -2,6 +2,6 @@ class role::manager_server {
   include ::profile::base_linux
   include ::profile::dns::client
   include ::profile::consul::server
-  include ::profile::wordpress
+  #include ::profile::wordpress
   include ::profile::docker_swarm
 }
