a4305e4c77738efe808e8854f0f82af652ca71cb
Monastyr <60261397+Monastyr@users.noreply.github.com>
2020-10-23 00:53:16 +0200

    Update docker_swarm.pp

--- site-modules/profile/manifests/docker_swarm.pp
+++ site-modules/profile/manifests/docker_swarm.pp
@@ -1,9 +1,11 @@
 class profile::docker_swarm {
 
+$dir_ip = lookup( 'Address', undef, undef, '1.1.1.1' )
+
 docker::swarm {'cluster_manager':
   init           => true,
-  advertise_addr =>  '$::ipaddress',
-  listen_addr    =>  '$::ipaddress' ,
+  advertise_addr =>  $dir_ip,
+  listen_addr    =>  $dir_ip ,
 }
   
 
