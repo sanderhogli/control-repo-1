3001d9839b2ada8fd1d45dbf76ea5e88c2c921b9
Monastyr <60261397+Monastyr@users.noreply.github.com>
2020-10-22 16:57:39 +0200

    Update wordpress.pp

--- site-modules/profile/manifests/wordpress.pp
+++ site-modules/profile/manifests/wordpress.pp
@@ -1,9 +1,21 @@
 class profile::wordpress { 
 
-docker::run { 'helloworld':
-  image   => 'ubuntu:precise',
-  command => '/bin/sh -c "while true; do echo hello world; sleep 1; done"',
-  volumes => ['my-volume:/var/log'],
-}
+	class {'docker::compose':
+	  ensure => present,
+	}
 
+	file { '/tmp/docker-compose.yml':
+		ensure  => present,
+		content => "
+		compose_test:
+			image: ubuntu:14.04
+			command: /bin/sh -c "while true; do echo hello world; sleep 1; done"
+		"
+	}
+	
+	docker_compose { 'test':
+		compose_files => ['/tmp/docker-compose.yml'],
+		ensure  => present,
+	}
+	
 }
