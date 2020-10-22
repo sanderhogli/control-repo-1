bee69763eaa05d3b1f8de745b7cf818322d3c34c
Monastyr <60261397+Monastyr@users.noreply.github.com>
2020-10-22 16:30:29 +0200

    Update wordpress.pp

--- site-modules/profile/manifests/wordpress.pp
+++ site-modules/profile/manifests/wordpress.pp
@@ -1,54 +1,9 @@
 class profile::wordpress { 
 
-  class { 'docker':
-    dns => '129.241.0.201',
-  }
-
-  class {'docker::compose':
-    ensure  => present,
-    version => '1.14.0',
-  }
-
-  file { '/tmp/docker-compose.yml':
-    ensure  => present,
-    content => "
-version: '3.3'
-
-services:
-   db:
-     image: mysql:5.7
-     volumes:
-       - db_data:/var/lib/mysql
-     restart: always
-     environment:
-       MYSQL_ROOT_PASSWORD: password
-       MYSQL_DATABASE: wordpress
-       MYSQL_USER: wordpress
-       MYSQL_PASSWORD: password
-
-   wordpress:
-     depends_on:
-       - db
-     image: wordpress:latest
-     ports:
-       - \"8000:80\"
-     restart: always
-     environment:
-       WORDPRESS_DB_HOST: db:3306
-       WORDPRESS_DB_USER: wordpress
-       WORDPRESS_DB_PASSWORD: password
-volumes:
-    db_data: {}
-"
-  }
-
-  docker_compose { '/tmp/docker-compose.yml':
-    ensure  => present,
-    require  => [ File['/tmp/docker-compose.yml'], 
-                  Class['docker::compose'], 
-                  Class['docker'], ],
-  }
-	
-
+docker::run { 'helloworld':
+  image   => 'ubuntu:precise',
+  command => '/bin/sh -c "while true; do echo hello world; sleep 1; done"',
+  volumes => ['my-volume:/var/log'],
+}
 
 }
