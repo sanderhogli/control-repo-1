8bec11e9ec2c6ac8239822cfe19d73c52623e928
Monastyr <60261397+Monastyr@users.noreply.github.com>
2020-10-23 16:23:31 +0200

    Update docker_swarm.pp

--- site-modules/profile/manifests/docker_swarm.pp
+++ site-modules/profile/manifests/docker_swarm.pp
@@ -11,13 +11,13 @@ class profile::docker_swarm {
 			  before => Exec[token]
 			  }
 			  
-		exec { 'token':
+			exec { 'token':
 				command => '/bin/echo -n "docker_swarm::token:" >> /etc/puppetlabs/code/shared-hieradata/common.yaml',
 				#path    => '/usr/local/bin/:/bin/',
 				# path    => [ '/usr/local/bin/', '/bin/' ],  # alternative synt  
 				}
 			exec { 'token2':
-				command => '/bin/echo  $(/usr/bin/docker swarm join-token worker | cut -d "," -f 3 ) >> /etc/puppetlabs/code/shared-hieradata/common.yaml',
+				command => '/bin/echo  $( /usr/bin/docker swarm join-token worker | cut -d "," -f 3 ) >> /etc/puppetlabs/code/shared-hieradata/common.yaml',
 				#     => '/usr/local/bin/:/bin/',
 				# path    => [ '/usr/local/bin/', '/usr/bin/docker' ],  # alternative synt
 				}
