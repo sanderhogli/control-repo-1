d2c8abef292b9c37e8cb88b8777d448085e94d4b
Monastyr <60261397+Monastyr@users.noreply.github.com>
2020-10-23 13:26:05 +0200

    a

--- site-modules/profile/manifests/docker_swarm.pp
+++ site-modules/profile/manifests/docker_swarm.pp
@@ -1,24 +1,12 @@
 class profile::docker_swarm {
-	class { 'docker':}
+class {'docker_swarm':}
 
-
-	if $hostname == 'manager' {
-			docker::swarm {'cluster_manager':
-			  init           => true,
-			  advertise_addr =>  $ipaddress,
-			  listen_addr    =>  $facts['networking']['ip'] ,
-			}
-	} else {
-	
-	docker::swarm {'cluster_worker':
-	  join           => true,
-	  advertise_addr => '192.168.1.2',
-	  listen_addr    => '192.168.1.2',
-	  manager_ip     => '192.168.1.1',
-	  token          => '$facts[docker_worker_join_token]',
-	}
-	
-	
-	}
+swarm_cluster {'cluster 1':
+  ensure       => present,
+  backend      => 'swarm',
+  cluster_type => 'create',
+  } 
 		
 }
+
+
